29e05c20082801c2b91d9e751052762c0b745641
andrew <andrew_zachary@outlook.com>
2022-10-16 19:30:35 +0200

    base vue/router/pinia

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterView } from 'vue-router'
+
+import Nav from './layout/nav.vue';
+</script>
+
+<template>
+  <header>
+    <Nav />
+  </header>
+
+  <RouterView />
+</template>
+
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {storeToRefs} from 'pinia';
+import {useCounterStore} from '../stores/counter';
+
+const store = useCounterStore();
+
+// extract state/getters
+const {count} = storeToRefs(store);
+// extract actions
+const {increment} = store;
+
+</script>
+
+<template>
+  <div id="home">
+    <h1>This is a home page {{count}}</h1>
+    <button @click="increment">increment</button>
+  </div>
+</template>
+
+<style>
+</style>

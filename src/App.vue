ba8b27c4069266248749572b05ae9bf4cf4cc0c8
andrew <andrew_zachary@outlook.com>
2022-10-17 00:00:00 +0000

    create footer, styling footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { RouterView } from 'vue-router';
 
+import gear from './assets/gear.svg';
+
 import Nav from './layout/nav.vue';
 </script>
 
 <template>
-  <div id="app-container" class="w-full">
+  <div id="app-container" class="w-full relative">
     <header>
       <Nav />
     </header>
@@ -16,7 +18,21 @@ import Nav from './layout/nav.vue';
         </transition>
       </RouterView>
     </main>
+    <footer class="w-full flex justify-end absolute bottom-0">
+      <img class="cursor-pointer" :src="gear" alt="gear">
+    </footer>
   </div>
 </template>
 
-<style></style>
+<style lang="scss">
+  #app-container {
+
+    footer {
+      @include withRtl('padding', 2rem 2rem 2rem 0rem, 2rem 0rem 2rem 2rem);
+  
+      img {
+        width: 4rem;
+      }
+    }
+  }
+</style>
--- src/layout/app-view.vue
+++ src/layout/app-view.vue
@@ -1,23 +1,23 @@
 <script setup></script>
 
 <template>
-    <section data-simplebar data-simplebar-auto-hide="false">
-        <header>
-          <h1 class="text-6xl font-bold font-pop text-primary capitalize">
-            <slot name="header"></slot>
-          </h1>
-        </header>
-        <slot name="content"></slot>
-    </section>
+  <section data-simplebar data-simplebar-auto-hide="false">
+    <header>
+      <h1 class="mb-8 text-6xl font-bold font-pop text-primary capitalize">
+        <slot name="header"></slot>
+      </h1>
+    </header>
+    <slot name="content"></slot>
+  </section>
 </template>
 
-<style scoped lang="scss">
+<style lang="scss">
   .height-adapt-enter-from, .height-adapt-leave-to {
     opacity: 0;
     max-height: 0;
   }
   .height-adapt-enter-active {
-    transition: all 0.5s ease-in-out;
+    transition: all 0.5s ease-out;
   }
   .height-adapt-leave-active {
     transition: all 0.3s ease-out;
@@ -32,10 +32,12 @@
     opacity: 1;
     width: 100%;
 
-    @include withRtl('padding', 0 0 0 1rem, 0 1rem 0 0);
+    @include withRtl('padding', 0 0 8rem 1rem, 0 1rem 8rem 0);
 
-    h1 {
-      margin-bottom: 1rem;
+    .simplebar-scrollbar::before {
+      background-color: $primary;
+      opacity: 1!important;
+      width: 0.4rem!important;
     }
   }
 </style>

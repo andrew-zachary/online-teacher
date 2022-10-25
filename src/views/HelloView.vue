af35c022a9d7f7e3f22b5c873c7862dd712816d7
andrew <andrew_zachary@outlook.com>
2022-10-25 00:00:00 +0000

    translate hello view

--- src/views/HelloView.vue
+++ src/views/HelloView.vue
@@ -2,16 +2,20 @@
 import View from '../layout/view.vue';
 import Scroller from '../layout/scroller.vue';
 
+import Translate from '../components/translate.vue';
+
 const id = "hello"
 </script>
 <template>
     <View :id="id">
         <Scroller>
             <template #header>
-                hello
+                <Translate toTranslate="hello.title" />
             </template>
             <template #content>
-                hello
+                <p class="mt-8 text-4xl font-popp font-normal capitalize">
+                    account settings ...
+                </p>
             </template>
         </Scroller>
     </View>

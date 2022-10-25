be6fe2035185873522c4f900ad377d90b63b235a
andrew <andrew_zachary@outlook.com>
2022-10-25 00:00:00 +0000

    new lesson form with ar/en and store

--- src/views/NewLessonView.vue
+++ src/views/NewLessonView.vue
@@ -2,18 +2,26 @@
 import View from '../layout/view.vue';
 import Scroller from '../layout/scroller.vue';
 
+import Translate from '../components/translate.vue';
+import NewLessonForm from '../components/new-lesson-form.vue';
+
 const id = "new-lesson"
 </script>
 <template>
     <View :id="id">
         <Scroller>
             <template #header>
-                new lesson
+                <Translate toTranslate="new_lesson.title" />
             </template>
             <template #content>
-                new lesson
+                <NewLessonForm />
             </template>
         </Scroller>
     </View>
 </template>
-<style></style>
+<style lang="scss">
+    #new-lesson {
+        max-width: 41.2rem;
+        margin: 0 auto;
+    }
+</style>

897c9ff95c967ffeab8c43d60065b31cd961fef5
andrew <andrew_zachary@outlook.com>
2022-10-28 00:00:00 +0000

    add fade-in-out animation PostsView RouterView

--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -3,7 +3,23 @@ import { RouterView } from 'vue-router';
 </script>
 
 <template>
-    <RouterView />
+    <div id="posts">
+        <RouterView v-slot="{Component}">
+            <transition name="posts-edit" mode="out-in">
+                <component :is="Component"></component>
+            </transition>
+        </RouterView>
+    </div>
 </template>
 
-<style></style>
+<style lang="scss">
+.posts-edit-enter-from, .posts-edit-leave-to {
+    opacity: 0;
+}
+.posts-edit-enter-active, .posts-edit-leave-active {
+    transition: all 0.5s ease-out;
+}
+.posts-edit-enter-to, .posts-edit-leave-from {
+    opacity: 1;
+}
+</style>

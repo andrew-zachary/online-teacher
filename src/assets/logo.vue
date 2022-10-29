13fff31c3a8facbb92baa88ad87bf625c04f905f
andrew <andrew_zachary@outlook.com>
2022-10-29 13:31:49 +0200

    edit arrow-down and other svg

--- src/assets/logo.vue
+++ src/assets/logo.vue
@@ -1,4 +1,3 @@
-<script></script>
 <template>
     <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
         width="696.662px" height="696.663px" viewBox="0 0 696.662 696.663" style="enable-background:new 0 0 696.662 696.663;"
@@ -17,4 +16,3 @@
         </g>
     </svg>
 </template>
-<style></style>
--- src/components/lang-switch.vue
+++ src/components/lang-switch.vue
@@ -1,6 +1,8 @@
 <script setup>
 import {ref} from 'vue';
 
+import ArrowDown from '../assets/arrow-down.vue';
+
 const {currentLang, langs} = defineProps(['currentLang', 'langs']);
 const emit = defineEmits(['langPicked']);
 
@@ -20,7 +22,7 @@ const pickingLang = (langIndex) => {
             class="flex items-center justify-between"
             @click="open = !open">
             <div class="text-2xl font-popp font-regular">{{pickedLang.name}}</div>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 510"><!--! Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 246.6l-127.1 128C176.4 380.9 168.2 384 160 384s-16.38-3.125-22.63-9.375l-127.1-128C.2244 237.5-2.516 223.7 2.438 211.8S19.07 192 32 192h255.1c12.94 0 24.62 7.781 29.58 19.75S319.8 237.5 310.6 246.6z"/></svg>
+            <Component :is="ArrowDown" />
         </div>
         <ul v-if="open" id="langs-list" class="pt-2">
             <li class="mb-2 cursor-pointer text-2xl font-popp font-regular" v-for="lang of langs" :key="lang.locale" @click="pickingLang(lang.index)">

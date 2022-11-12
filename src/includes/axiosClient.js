d761a52c821358cd2d657ba3f16fdfe723b3f57b
andrew <andrew_zachary@outlook.com>
2022-11-12 17:51:41 +0200

    update global header x-app-key

--- src/includes/axiosClient.js
+++ src/includes/axiosClient.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const baseURL = window.location.host.includes('localhost')?'http://localhost:3000/api':'https://andrew-zachary-dev.onrender.com/api';
 const axiosClient = axios.create({
+    headers: {
+        'x-app-key': import.meta.env.VITE_APP_KEY
+    },
     baseURL,
     withCredentials: true
 });

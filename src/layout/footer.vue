5bb0fd11c3d7ccf46763b938c72596647fc3929c
andrew <andrew_zachary@outlook.com>
2022-11-14 21:01:14 +0200

    mobile fix#2 footer translate -30px at min-width:768px

--- src/layout/footer.vue
+++ src/layout/footer.vue
@@ -12,8 +12,11 @@ import Settings from '../components/settings.vue';
 footer {
     @include withRtl('left', 100%, unset);
     @include withRtl('right', unset, 100%);
-    @include withRtl('transform', translate(-120%, 0%), translate(120%, 0%));
-  
+    @include withRtl('transform', translate(-120%, -30px), translate(120%, -30px));
+    @media screen and (min-width: 768px) {
+        @include withRtl('transform', translate(-120%, 0%), translate(120%, 0%));
+    }
+
     img {
         width: 4rem;
     }

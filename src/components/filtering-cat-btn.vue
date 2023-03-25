bfa0c0de9b07fa9f7def1a863cfd7e4351e28dac
andrew-zachary <andrew_zachary@outlook.com>
2023-03-25 00:00:00 +0000

    fix filtering-cat-btn text alignment in RTL

--- src/components/filtering-cat-btn.vue
+++ src/components/filtering-cat-btn.vue
@@ -7,16 +7,29 @@
     defineProps(['filteringCat']);
 </script>
 <template>
-    <div class="px-6 py-2
+    <div class="filtering-cat-btn px-6 py-2
         inline-block
-        text-3xl text-primary dark:text-secondary capitalize font-mont font-bold">
+        text-3xl text-primary dark:text-secondary capitalize font-mont font-bold"
+    >
         <div v-if="filteringCat.slug !== ''"
-            class="flex items-center border-2 border-primary dark:border-primary-dark py-1 px-4 cursor-pointer rounded-full"
+            class="flex border-2 border-primary dark:border-primary-dark py-1 px-4 cursor-pointer rounded-full"
             @click="emits('resetFiltering')">
-            {{ filteringCat.slug }} <BtnIcon :icon="close" class="p-0" />
+            <span>{{ filteringCat.slug }}</span> <BtnIcon :icon="close" class="p-0" />
         </div>
         <div v-else class="py-2 px-4">
             {{ 'all' }}
         </div>
     </div>
 </template>
+<style lang="scss">
+    .filtering-cat-btn {
+        
+        div {
+            
+            span {
+                @include withRtl('align-self', center, flex-end);
+                @include withRtl('line-height', 1, 0.9);
+            }
+        }
+    }
+</style>
